11c4935f67b5264e12f81856ce3bb73675c7c423
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-26 22:31:01 +0300

    Getting reference to HTML elements

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentloaded", () =>{
     const BASE_URL = "http://localhost:3000/users";
+
+    //Getting reference to HTML elements
+    const userNameInput = document.getElementById("user-name");
+    const incomeInput = document.getElementById("income");
+    const expenditureNameInput = document.getElementById("exipenditure-name");
+    const expenditureAmountInput = document.getElementById("expenditure-amount");
+    const totalIncomeDisplay = document.getElementById("totalIncome");
+    const totalExpenditureDisplay = document.getElementById("totalExpenditure");
+    const summaryDiv = document.getElementById("summary");
+    const alertBox = document.getElementById("alertBox")
 })
